eca1e6f564b7ebe55d90804689cd9a6a7d0cc05d
Janak Maloney <32804789+DracoKali@users.noreply.github.com>
2018-01-08 15:13:25 -0500

    Add files via upload

---
+++ FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram{
+	public static void main(String[] args){
+		System.out.println("My name is Janak");  
+        System.out.println("I am 26");
+        System.out.println("My hometown is Silver Spring, MD");
+	}
+}
