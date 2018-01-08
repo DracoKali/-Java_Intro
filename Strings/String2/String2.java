b6d5acc79e341ad6c671ace24bd14fb5d261252e
Janak Maloney <32804789+DracoKali@users.noreply.github.com>
2018-01-08 15:22:33 -0500

    Add files via upload

---
+++ Strings/String2/String2.java
@@ -0,0 +1,8 @@
+public class String2 {
+     public static void main(String[] args) {
+        String string1 = "My name is ";
+		String string2 = "Michael";
+		String string3 = string1.concat(string2);
+		System.out.println(string3);
+     }
+}
---
+++ Strings/String3/String3.java
@@ -0,0 +1,6 @@
+public class String3 {
+     public static void main(String[] args) {
+     	String ninja = String.format("Hi %s, you owe me $%.2f !", "Jack", 25.);
+		System.out.println(ninja);
+     }
+}
